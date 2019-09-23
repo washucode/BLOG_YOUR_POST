eb974107f4b2d8523a80ee3f1604bb5a0dd637f6
washucode <estherw8525@gmail.com>
2019-09-24 02:09:38 +0300

    start deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export SECRET_KEY=435313ea80b5a872114356a1
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL='postgresql+psycopg2://esther:p@localhost/blog'
-python3 manage.py server
+# python3 manage.py server
+# python manage.py db migrate -m "Add class Quotes"
+python manage.py db upgrade
