195f6529854a4a26f9cccb2d0878b1fd25bb0bd9
washucode <estherw8525@gmail.com>
2019-09-24 03:47:40 +0300

    heroku deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=435313ea80b5a872114356a1
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL='postgresql+psycopg2://esther:p@localhost/blog'
-python3 manage.py server
+# python3 manage.py shell
 # python manage.py db migrate -m "Deployment"
-# python manage.py db upgrade
+python manage.py db upgrade
