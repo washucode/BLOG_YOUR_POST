8e7fc51827f94e74248d2c50e83a1f979bd3cdf3
washucode <estherw8525@gmail.com>
2019-09-24 10:04:33 +0300

    fix a bug3

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,13 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES 
 from flask_mail import Mail
 
 
 app = Flask(__name__)
-bootstrap = Bootstrap()
+# bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
@@ -26,7 +26,7 @@ def  create_app():
     # initialize extensions
     login_manager.init_app(app)
     db.init_app(app)
-    bootstrap.init_app(app)
+    # bootstrap.init_app(app)
     mail.init_app(app)
 
      # configure UploadSet
